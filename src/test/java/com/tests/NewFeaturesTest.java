714c2fea47e5a613314c965f913eddee4bc0f9ed
JeffreyHeytow <heytow1@gmail.com>
2019-07-15 15:42:00 -0500

    updated pom and properties files

--- src/test/java/com/tests/NewFeaturesTest.java
+++ src/test/java/com/tests/NewFeaturesTest.java
@@ -1,29 +1,34 @@
 package com.tests;
 
+import com.usf.metadata.Metadata;
+import com.usf.utils.parsers.Parser;
 import org.testng.annotations.Test;
 import test.BaseUITest;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.usf.utils.ConfigurationReader.getConfigValue;
 
 
 public class NewFeaturesTest extends BaseUITest {
 
     @Test
     public void featureTest(){
-        System.out.println("<------------------------------------------->");
-        System.out.println();
-        System.out.println("CORE PROPERTIES:");
-        properties.getCoreConfigs().forEach((key, value) -> System.out.println(key + ":" + value));
-        System.out.println();
-        System.out.println("<------------------------------------------->");
-        System.out.println();
-        System.out.println("CLIENT PROPERTIES:");
-        properties.getClientConfigs().forEach((key, value) -> System.out.println(key + ":" + value));
-        System.out.println();
-        System.out.println("<------------------------------------------->");
-        System.out.println();
-        System.out.println("METADATA-LOCATION");
-        System.out.println(properties.getConfigValue("metadata-location"));
-        System.out.println();
-        System.out.println("<------------------------------------------->");
+        Parser parser = new Parser(getConfigValue("metadata-path"), "metadata.xml");
 
+        parser.parseFile();
+
+        System.out.println(Metadata.getInstance().getValue("username"));
+
+
+        Iterator it = Metadata.getInstance().getMetadata().entrySet().iterator();
+        System.out.println();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            System.out.print("METADATA ~~>\t" + pair.getKey() + ": " + pair.getValue() + "\n");
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        System.out.println();
     }
 }
